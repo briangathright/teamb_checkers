76ef16c4407b9a4aaae6a71443d5077d8dcc9994
fatmajaddoh <fjaddoh@luc.edu>
2015-02-21 18:44:19 -0600

    new interface

--- Checker/app/src/main/java/com/example/fatmajaddoh/checker/MainActivity.java
+++ Checker/app/src/main/java/com/example/fatmajaddoh/checker/MainActivity.java
@@ -1,103 +1,203 @@
 package com.example.fatmajaddoh.checker;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Display;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TableRow;
-import android.app.Activity;
-import android.util.DisplayMetrics;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Point;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.content.ClipData;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.view.View.DragShadowBuilder;
 
 
 
 public class MainActivity extends Activity {
+    String msg;
 
-    private static final int num_row=8;
-    private static final int num_col=8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        populateButtons();
+        GridView celllayout = (GridView) findViewById(R.id.CHECKERBOARD);
+
+        celllayout.setAdapter(new SquareAdapter(this));
+
+
     }
 
 
+    public class SquareAdapter extends BaseAdapter {
+
+        Context mContext;
+
+        private int[] boardIds = {
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+                R.drawable.darksquare, R.drawable.lightsquare, R.drawable.darksquare, R.drawable.lightsquare,
+        };
 
 
+        public SquareAdapter(Context c) {
+            mContext = c;
 
 
-    private void populateButtons() {
+        }
 
-       int cellWidth  =100;
-       int cellHeigth  =100;
-       //get screen size
-        Display display =  getWindowManager().getDefaultDisplay();
-        Point size =new Point();
-        display.getSize(size);
-        int height = size.y;
-        int Width = size.x;
-        int rows= height/cellHeigth;
-       int columns = Width/cellWidth;
+        @Override
+        public int getCount() {
+            return boardIds.length;
+        }
 
-       LinearLayout celllayout=(LinearLayout)findViewById(R.id.cellLayout);
+        @Override
+        public Object getItem(int arg0) {
+            return null;
+        }
 
-        Board [][] cellViews= new Board [rows][rows];
-        for(int row =0;row<num_row;row++){
+        @Override
+        public long getItemId(int arg0) {
+            return 0;
+        }
 
-        LinearLayout rowLinearLayout=new LinearLayout(getApplicationContext());
-            rowLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
 
+            View squareContainerView = convertView;
 
-         Board[] rowArray=new Board[columns];
-          for(int col=0;col<num_col;col++){
-            Board button=new Board(this,row,col);
-             button.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
-                    TableRow.LayoutParams.MATCH_PARENT,70.0f));
-             //Cell coloring
-              if(row%2==0 )
-              if (col%2==0 )
-               button.setBackgroundColor(getResources().getColor(R.color.Black));
-               else  button.setBackgroundColor(getResources().getColor(R.color.white));
-            else
-             if (col%2!=0)
-                 button.setBackgroundColor(getResources().getColor(R.color.Black));
-              else  button.setBackgroundColor(getResources().getColor(R.color.white));
+            if (convertView == null) {
+                //Inflate the layout
+                final LayoutInflater layoutInflater = (LayoutInflater)
+                        this.mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+                squareContainerView =
+                        layoutInflater.inflate(R.layout.square, null);
 
-              rowLinearLayout.addView(button);
-              rowArray[col]=button;
-          // Placing black pieces in the board
-              if (row==0||row==2)
-                 if (col%2==0 )
-              button.setBackgroundResource(R.drawable.checker);
-             if(row==1)
-                if (! (col%2==0 ))
-                      button.setBackgroundResource(R.drawable.checker);
+                // Background
+                final ImageView squareView =
+                        (ImageView) squareContainerView.findViewById(R.id.square_background);
+                squareView.setOnDragListener(new MyDragListener());
+                squareView.setPadding(-20, -20, -20, -20);
+                squareView.setImageResource(boardIds[position]);
 
+                if (boardIds[position] == R.drawable.darksquare) {
+                    if (position <= 23) {
+                        final ImageView pieceView =
+                                (ImageView) squareContainerView.findViewById(R.id.piece);
+                        pieceView.setOnTouchListener(new MyTouchListener());
+                        pieceView.setImageResource(R.drawable.checker);
+                        pieceView.setTag(position);
+                    } else if (position > 39) {
+                        final ImageView pieceView =
+                                (ImageView) squareContainerView.findViewById(R.id.piece);
+                        pieceView.setOnTouchListener(new MyTouchListener());
+                        pieceView.setImageResource(R.drawable.redpicese);
+                        pieceView.setTag(position);
+                    }
 
-            // Placing red pieces in the board
-              if (row==5||row==7)
-                  if (col%2!=0)
-                      button.setBackgroundResource(R.drawable.redpicese);
-
-              if(row==6)
-                  if (col%2==0 )
-                      button.setBackgroundResource(R.drawable.redpicese);
+                }
 
 
+            }
 
-          }
-            cellViews[row]=rowArray;
-            celllayout.addView(rowLinearLayout);
-           }}
 
+            return squareContainerView;
+        }
+    }
+
+
+    private final class MyTouchListener implements OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
+    class MyDragListener implements View.OnDragListener {
+
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            event.getAction();
+
+            switch (event.getAction()) {
+                  /*    case DragEvent.ACTION_DRAG_STARTED:
+                       // layoutParams = (GridView.LayoutParams) v.getLayoutParams();
+
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                      //  int x = (int) event.getX();
+                        //int y = (int) event.getY();
+                      //  break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                       // x = (int) event.getX();
+                      //  y = (int) event.getY();
+                      //  layoutParams.l = x;
+                      //  layoutParams.topMargin = y;
+                        v.setLayoutParams(layoutParams);
+                        break;
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                      //  x = (int) event.getX();
+                     //   y = (int) event.getY();
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+
+                        break;*/
+                case DragEvent.ACTION_DROP:
+                    Log.d(msg, "Dropped");
+                        /*View view = (View) event.getLocalState();
+                        ViewGroup owner = (ViewGroup) view.getParent();
+
+                        owner.removeView(view);
+
+                        FrameLayout container = (FrameLayout) v;
+                        container.addView(view);
+
+                       view.setVisibility(View.VISIBLE);*/
+
+
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -120,4 +220,13 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
+
+
+
+
+
