30137a0c56f0905f450ad9a810cde965f077f769
Thiago Puluceno <puluceno@gmail.com>
2015-04-23 12:38:15 -0500

    Setting up JUnit

---
+++ Checkers/app/src/androidTest/java/com/honigsheroes/checkers/model/test/CheckersGameTest.java
@@ -0,0 +1,35 @@
+package com.honigsheroes.checkers.model.test;
+
+import com.honigsheroes.checkers.Constants.PlayerColor;
+import com.honigsheroes.checkers.model.CurrentBoard;
+import com.honigsheroes.checkers.model.Square;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by pulu on 4/23/15.
+ */
+public class CheckersGameTest extends TestCase {
+
+    private PlayerColor playerTurn;
+    private CurrentBoard board;
+    private Square[] squares;
+
+    @Override
+    protected void setUp() throws Exception {
+        playerTurn= PlayerColor.BLACK;
+        board=new CurrentBoard(squares);
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testCheckWinConditions(){
+
+    }
+
+
+}
